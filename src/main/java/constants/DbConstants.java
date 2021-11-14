893f8b2233e103bdad4eec6c64f220dce54b2e5f
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-11-14 13:30:26 +0200

    added and tested DbConnection.java to make connection with data bases

---
+++ src/main/java/constants/DbConstants.java
@@ -0,0 +1,7 @@
+package constants;
+
+public interface DbConstants {
+    String DEFAULT_URL = "jdbc:h2:mem:db";
+    String DEFAULT_USER = "user";
+    String DEFAULT_PASSWORD = "password";
+}
---
+++ src/test/java/ConnectionTest.java
@@ -0,0 +1,35 @@
+import dbConnection.DbConnection;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ConnectionTest {
+
+
+    DbConnection defaultConnection = new DbConnection();
+
+    @Test
+    void createConnectionDefault() throws SQLException {
+        try(Connection connection = defaultConnection.getConnection()) {
+            assertTrue(connection.isValid(1));
+            assertFalse(connection.isClosed());
+        }
+    }
+
+    @Test
+    void createConnectionPostgre() throws SQLException {
+        String url ="jdbc:postgresql:postgreDb";
+        String user = "userPg";
+        String password ="userPg";
+        DbConnection customConnection = new DbConnection(url,user,password);
+
+        try(Connection connection = customConnection.getConnection()) {
+            assertTrue(connection.isValid(1));
+            assertFalse(connection.isClosed());
+        }
+    }
+}
